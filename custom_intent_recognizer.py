161793964fbcd9452932f98d85533a98b3e16680
lixavi <121849896+lixavi@users.noreply.github.com>
2021-10-21 13:01:17 +0530

    updated

---
+++ custom_intent_recognizer.py
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+# custom_intent_recognizer.py
+
+from rasa.nlu.components import Component
+from rasa.nlu import utils
+from rasa.nlu.model import Metadata
+from typing import Any, Optional, Text, Dict
+
+class CustomIntentRecognizer(Component):
+    """Custom intent recognizer"""
+
+    name = "custom_intent_recognizer"
+    provides = ["intent"]
+    requires = []
+    defaults = {}
+    language_list = ["en"]
+
+    def __init__(self, component_config: Optional[Dict[Text, Any]] = None) -> None:
+        super().__init__(component_config)
+
+    def train(self, training_data, cfg, **kwargs):
+        pass
+
+    def process(self, message, **kwargs):
+        intent = self.recognize(message.text)
+        message.set("intent", intent, add_to_output=True)
+
+    def persist(self, file_name: Text, model_dir: Text) -> Optional[Dict[Text, Any]]:
+        return {"file_name": file_name}
+
+    @classmethod
+    def load(cls, meta: Dict[Text, Any], mode
+    l_dir: Text = None, model_metadata: Metadata = None, 
+
+    cached_component: Optional["Component"] = None, **kwargs: Any) -> "Component":
+    
+
+    
+    
+        return cls(meta)
+        
+
+    
+    def recognize(self, text: Text) -> Optional[Text]:
+        
+        # Custom intent recognition log
+        # ic here
+
+        return "custom_intent"
+
